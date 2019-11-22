8b356513c7126f84accb9ba144850f0eecb7959a
Inhangá <nakappa1@hotmail.com>
2019-11-21 21:57:37 -0300

    Criando a classe ConvertBin.

---
+++ Conversor-BIN-DEC-HEX/Functions/ConvertBin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System //Conversor_BIN_DEC_HEX.Functions
+{
+    class ConvertBin
+    {
+        public void convertBin()
+        {
+            int contador = 0;
+            Console.WriteLine();
+            Console.Write("Digite um número binário: ");
+            Bina binar = new Bina();
+            binar.digito = Console.ReadLine();
+            int n = binar.digito.Length;
+            char[] s = new char[n];
+            s = binar.digito.ToCharArray();
+
+            while (contador == 0)
+            {
+                foreach (char w in s)
+                {
+                    if (w != '0' && w != '1') contador = 1;
+                }
+
+                if (contador == 1)
+                {
+                    Console.WriteLine("FAVOR DIGITAR ALGARISMOS ENTRE 0 OU 1!!!");
+                    Console.WriteLine();
+                    Console.Write("Digite um número binário: ");
+                    binar.digito = Console.ReadLine();
+                    Console.WriteLine();
+                    Console.WriteLine("Número digitado é: " + binar.digito);
+                    n = binar.digito.Length;
+                    s = new char[n];
+                    s = binar.digito.ToCharArray();
+                    contador = 0;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.Write("Número digitado é: " + binar.digito);
+                    contador = 1;
+                }
+            }
+
+            Console.WriteLine();
+            binar.deci();
+            Console.WriteLine("Número decimal: " + binar.y);
+            binar.hexa();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
--- Conversor-BIN-DEC-HEX/Program.cs
+++ Conversor-BIN-DEC-HEX/Program.cs
@@ -10,24 +10,8 @@ namespace Conversor_BIN_DEC_HEX
     {
         static void Main(string[] args)
         {
-            /*Bina binar = new Bina();
-            binar.digito = Console.ReadLine();
-            binar.deci();
-            Console.WriteLine("Número decimal: " + binar.y);
-            binar.hexa();*/
-
-            /*Deci decim = new Deci();
-            decim.digito = Console.ReadLine();
-            decim.bina();
-            Console.WriteLine();
-            decim.hexa();*/
-
-            Hexa hexad = new Hexa();
-            hexad.digito = Console.ReadLine();
-            hexad.bina();
-            Console.WriteLine();
-            hexad.deci();
-            Console.WriteLine("Número decimal: " + hexad.y);
+            ConvertBin binar = new ConvertBin();
+            binar.convertBin();
 
             Console.ReadLine();
         }
