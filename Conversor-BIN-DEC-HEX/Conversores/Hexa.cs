f9951a6bb1592906b09acad3a526c0459701ef8f
Inhangá <nakappa1@hotmail.com>
2019-11-21 21:27:16 -0300

    Criando a classe Hexa.

---
+++ Conversor-BIN-DEC-HEX/Conversores/Hexa.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace System //Conversor_BIN_DEC_HEX.Conversores
+{
+    class Hexa
+    {
+        public List<int> list = new List<int>();
+        public string digito;
+        public int valor = 0;
+        public int cont = 0;
+        public double y;
+        public int giro = 0;
+
+        public void deci()
+        {
+            int n = digito.Length;
+            char[] s = new char[n];
+            s = digito.ToCharArray();
+            double[] x = new double[n];
+
+            foreach (char w in s)
+            {
+                int z = giro;
+                y = n - giro - 1;
+
+                switch (s[z])
+                {
+                    case '0':
+                        valor = 0;
+                        break;
+
+                    case '1':
+                        valor = 1;
+                        break;
+
+                    case '2':
+                        valor = 2;
+                        break;
+
+                    case '3':
+                        valor = 3;
+                        break;
+
+                    case '4':
+                        valor = 4;
+                        break;
+
+                    case '5':
+                        valor = 5;
+                        break;
+
+                    case '6':
+                        valor = 6;
+                        break;
+
+                    case '7':
+                        valor = 7;
+                        break;
+
+                    case '8':
+                        valor = 8;
+                        break;
+
+                    case '9':
+                        valor = 9;
+                        break;
+
+                    case 'a':
+                        valor = 10;
+                        break;
+
+                    case 'A':
+                        valor = 10;
+                        break;
+
+                    case 'b':
+                        valor = 11;
+                        break;
+
+                    case 'B':
+                        valor = 11;
+                        break;
+
+                    case 'c':
+                        valor = 12;
+                        break;
+
+                    case 'C':
+                        valor = 12;
+                        break;
+
+                    case 'd':
+                        valor = 13;
+                        break;
+
+                    case 'D':
+                        valor = 13;
+                        break;
+
+                    case 'e':
+                        valor = 14;
+                        break;
+
+                    case 'E':
+                        valor = 14;
+                        break;
+
+                    default:
+                        valor = 15;
+                        break;
+                }
+                x[z] = valor * Math.Pow(16, y);
+
+                giro++;
+            }
+
+            giro = 0;
+            y = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                y += x[i];
+            }
+        }
+
+        public void bina()
+        {
+            deci();
+            Deci bina = new Deci();
+            bina.digito = y.ToString();
+            bina.bina();
+
+        }
+
+    }
+}
--- Conversor-BIN-DEC-HEX/Program.cs
+++ Conversor-BIN-DEC-HEX/Program.cs
@@ -10,11 +10,24 @@ namespace Conversor_BIN_DEC_HEX
     {
         static void Main(string[] args)
         {
-            Bina binar = new Bina();
+            /*Bina binar = new Bina();
             binar.digito = Console.ReadLine();
             binar.deci();
             Console.WriteLine("Número decimal: " + binar.y);
-            binar.hexa();
+            binar.hexa();*/
+
+            /*Deci decim = new Deci();
+            decim.digito = Console.ReadLine();
+            decim.bina();
+            Console.WriteLine();
+            decim.hexa();*/
+
+            Hexa hexad = new Hexa();
+            hexad.digito = Console.ReadLine();
+            hexad.bina();
+            Console.WriteLine();
+            hexad.deci();
+            Console.WriteLine("Número decimal: " + hexad.y);
 
             Console.ReadLine();
         }
